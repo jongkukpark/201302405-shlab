7cdd1bd7f0c7ff5396fef8e51fd009d5cfa04eec
jongkukpark <trly12@cnu.ac.kr>
2017-11-20 16:10:20 +0900

    trace02

--- tsh.c
+++ tsh.c
@@ -170,9 +170,19 @@ int main(int argc, char **argv)
 void eval(char *cmdline) 
 {
 	char *argv[MAXARGS];
+	pid_t pid;
 
 	parseline(cmdline, argv);
-	builtin_cmd(argv);
+
+	if (!builtin_cmd(argv)) {
+		if ((pid=fork()) == 0) {
+			if (execve(argv[0], argv, environ) < 0) {
+				printf("%s: Command not found.\n", argv[0]);
+				exit(0);
+			}
+		}
+	}
+
 	return;
 }
 
